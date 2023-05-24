ec21fe62143b3087ff99911d5311ab04bee33977
Damien PERNIN <daz.oufzor@gmail.com>
2023-05-24 14:23:49 +0200

    tests contact 10

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,74 +1,55 @@
 import React, { useState } from 'react';
 
-export default function ContactForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Envoie des données du formulaire via une requête POST
-    fetch('/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: encodeFormData({ name, email, message })
-    })
-      .then(() => {
-        // Succès de l'envoi de l'e-mail
-        alert('Votre message a été envoyé avec succès.');
+const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Effectuez ici l'envoi des données du formulaire, par exemple via une API
+        
+        // Réinitialisez les champs du formulaire après l'envoi
         setName('');
         setEmail('');
         setMessage('');
-      })
-      .catch(error => {
-        console.error('Une erreur s\'est produite:', error);
-        alert('Une erreur s\'est produite lors de l\'envoi du message. Veuillez réessayer.');
-      });
-  };
-
-  const encodeFormData = (data) => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&');
-  };
-
-  return (
-    <form name="contact-v1" data-netlify="true" onSubmit={handleSubmit}>
-      <div>
+    };
+    
+    return (
+        <form name="contact-portfolio" method="post" onSubmit={handleSubmit}>
+            <input type="hidden" name="form-name" value="contact-portfolio" />
+        <div>
         <label htmlFor="name">Nom :</label><br />
         <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
         />
-      </div>
-      <div>
+        </div>
+        <div>
         <label htmlFor="email">Email :</label><br />
         <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-      <div>
+        </div>
+        <div>
         <label htmlFor="message">Message :</label><br />
         <textarea
-          id="message"
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
         ></textarea>
-      </div>
-      <center>
+        </div>
+        <center>
         <button type="submit">Envoyer</button>
-      </center>
-    </form>
-  );
-};
+        </center>
+        </form>
+        );
+    };
+    
+    export default ContactForm;
