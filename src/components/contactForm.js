d5ad3f7b81b1c44090139063373feafa4b08b85b
Damien PERNIN <daz.oufzor@gmail.com>
2023-05-24 14:26:10 +0200

    tests contact 10

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,55 +1,24 @@
-import React, { useState } from 'react';
+import React from "react";
 
-const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        // Effectuez ici l'envoi des données du formulaire, par exemple via une API
-        
-        // Réinitialisez les champs du formulaire après l'envoi
-        setName('');
-        setEmail('');
-        setMessage('');
-    };
-    
-    return (
-        <form name="contact-portfolio" method="post" onSubmit={handleSubmit}>
-            <input type="hidden" name="form-name" value="contact-portfolio" />
-        <div>
-        <label htmlFor="name">Nom :</label><br />
-        <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-        />
-        </div>
-        <div>
-        <label htmlFor="email">Email :</label><br />
-        <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-        />
-        </div>
-        <div>
-        <label htmlFor="message">Message :</label><br />
-        <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        </div>
-        <center>
-        <button type="submit">Envoyer</button>
-        </center>
-        </form>
-        );
-    };
-    
-    export default ContactForm;
+export default function Form() {
+  return (
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Name</label> <br />
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Submit message" />
+      </p>
+    </form>
+  );
+}
