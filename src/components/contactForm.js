786b332661e33121110d06010b8be040f2e28f56
Damien PERNIN <daz.oufzor@gmail.com>
2023-05-24 13:31:59 +0200

    tests contact 9b

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,36 +1,71 @@
-import React from "react";
+import React, { useState } from 'react';
 
-export default function contactForm() {
-    return (
-        <div>
-            <form
-                name="contact portfolio"
-                method="post"
-                netlify
-                netlify-honeypot="bot-field"
-                onSubmit="submit"
-            >
-                <input type="hidden" name="form-name" value="contact portfolio" />
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-                <div>
-                    <label htmlFor="nom">Nom
-                        <input id="nom" type="text" name="nom" />
-                    </label>
-                </div>
-                <div>
-                    <label htmlFor="email">Email
-                        <input id="email" type="email" name="email" />
-                    </label>
-                </div>
-                <div>
-                    <label htmlFor="message">Message
-                        <textarea id="message" name="message"></textarea>
-                    </label>
-                </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                <button type="submit">Envoyer</button>
+    // Envoie des données du formulaire via une requête POST
+    fetch('../contact.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, message })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          // Succès de l'envoi de l'e-mail
+          alert('Votre message a été envoyé avec succès.');
+          setName('');
+          setEmail('');
+          setMessage('');
+        } else {
+          // Erreur lors de l'envoi de l'e-mail
+          alert('Une erreur s\'est produite lors de l\'envoi du message. Veuillez réessayer.');
+        }
+      })
+      .catch(error => {
+        console.error('Une erreur s\'est produite:', error);
+        alert('Une erreur s\'est produite lors de l\'envoi du message. Veuillez réessayer.');
+      });
+  };
 
-            </form>
-        </div>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nom :</label><br />
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email :</label><br />
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message :</label><br />
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+      </div>
+      <center>
+        <button type="submit">Envoyer</button>
+      </center>
+    </form>
+  );
+};
