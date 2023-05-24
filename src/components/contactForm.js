069f3276b1cbe6e76368824a86a4be389924beeb
Damien PERNIN <daz.oufzor@gmail.com>
2023-05-24 11:56:53 +0200

    tests contact 8

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,61 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
-
-  class ContactForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { name: "", email: "", message: "" };
-    }
-
-    /* Here’s the juicy bit for posting the form submission */
-
-    handleSubmit = e => {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...this.state })
-      })
-        .then(() => alert("Success!"))
-        .catch(error => alert(error));
-
-      e.preventDefault();
+const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Effectuez ici l'envoi des données du formulaire, par exemple via une API
+        
+        // Réinitialisez les champs du formulaire après l'envoi
+        setName('');
+        setEmail('');
+        setMessage('');
     };
-
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-    render() {
-      const { name, email, message } = this.state;
-      return (
-        <form name="contact" method="post" onSubmit={this.handleSubmit}>
+    
+    
+    return (
+        <form name="contact" method="post" onSubmit={handleSubmit} netlify>
             <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <div>
+        <label htmlFor="name">Nom :</label><br />
+        <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+        />
+        </div>
+        <div>
+        <label htmlFor="email">Email :</label><br />
+        <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        </div>
+        <div>
+        <label htmlFor="message">Message :</label><br />
+        <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        </div>
+        <center>
+        <button type="submit">Envoyer</button>
+        </center>
         </form>
-      );
-    }
-  }
-
-//   ReactDOM.render(<ContactForm />, document.getElementById("root"));
-
-export default ContactForm;
+        );
+    };
+    
+    export default ContactForm;
